1583ce25723210aabb484915af256a70c92b1e14
codeamac07 <codeamac07@gmail.com>
2015-09-04 13:34:40 -0500

    metodo completo

--- abuela.rb
+++ abuela.rb
@@ -1,2 +1,25 @@
 def deaf_grandma
+  print "TU: "
+  saludo=gets.chomp
+  enters=0
+  while saludo!="Abuelita te quiero, pero tengo que irme" && enters<2
+    if saludo!=""
+      enters=0
+      if saludo.upcase!
+        puts "ABUELA: HUH?! NO TE ESCUCHO, HIJO!"
+        print "TU: "
+        saludo=gets.chomp
+      else
+        puts "ABUELA: NO, NO DESDE 1983"
+        print "TU: "
+        saludo=gets.chomp
+      end
+    else
+      enters+=1
+      puts "NO ENTENDÍ!"
+      print "TU: "
+      saludo=gets.chomp 
+    end
+  end
 end
+deaf_grandma
